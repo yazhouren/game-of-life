fe27e77a4cad479700f41d468297b79e6812db39
Yazhou Ren <yazhouren@gmail.com>
2012-03-26 11:44:22 +0800

    changes xxzcell

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,7 +1,7 @@
 package com.wakaleo.gameoflife.domain;
 
-public enum Cell {
-    LIVE_CELL("****"), DEAD_CELL(".");
+public enum ZCell {
+    LIVE_CELL("*"), DEAD_CELL(".");
 
     private String symbol;
 
